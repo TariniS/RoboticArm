cbb59c9d0bf9f8aff3f62916cc8313430a18a57d
TariniS <tareene@gmail.com>
2016-07-30 14:43:02 -0700

    V1. Just getting started.

---
+++ RoboticArmMainCode.ino
@@ -0,0 +1,12 @@
+// This is going to be the main file.
+// 07/30/16: Write the first version where we try to move one motor 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
